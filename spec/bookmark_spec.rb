3107270387b59f76f49e18c302c78119a9694d74
Rich Ewin <richardewinlambert@gmail.com>
2020-06-02 17:08:24 +0100

    ammended tests

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -5,12 +5,11 @@ require './lib/bookmark'
 describe Bookmark do
   describe '.all' do
     it 'allows a user to view their bookmarks' do
-      # expect(subject).to respond_to(:all)
       bookmarks = Bookmark.all
 
-      expect(bookmarks).to include 'www.bbc.co.uk'
-      expect(bookmarks).to include 'www.skysports.co.uk'
-      expect(bookmarks).to include 'https://makers.tech/'
+      expect(bookmarks).to include 'http://www.makersacademy.com'
+      expect(bookmarks).to include 'http://www.google.com'
+      expect(bookmarks).to include 'http://www.destroyallsoftware.com'
     end
   end
 end
--- spec/features/view_bookmark_spec.rb
+++ spec/features/view_bookmark_spec.rb
@@ -8,8 +8,8 @@ feature 'viewing bookmarks' do
 
   scenario 'A user can view their bookmarks' do
     visit '/bookmarks'
-    expect(page).to have_content 'www.bbc.co.uk'
-    expect(page).to have_content 'www.skysports.co.uk'
-    expect(page).to have_content 'https://makers.tech/'
+    expect(page).to have_content 'http://www.makersacademy.com'
+    expect(page).to have_content 'http://www.google.com'
+    expect(page).to have_content 'http://www.destroyallsoftware.com'
   end
 end
